df59011832940ceea7ed3e5f3d82ec70598a7de8
Rakib Khan <rakib4kbd@gmail.com>
2025-12-04 13:00:59 +0600

    feat: new feature added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      MAJOR CHANGE
+      <h1>MAJOR CHANGE</h1>
+      <h1>FEATURE CAHNGES</h1>
     </div>
   );
 }
