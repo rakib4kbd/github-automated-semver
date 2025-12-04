8741cbe0b7e2b0a82c3d55bf68b1593d9dc0de90
Rakib Khan <rakib4kbd@gmail.com>
2025-12-04 13:04:11 +0600

    fix: spelling

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>MAJOR CHANGE</h1>
-      <h1>FEATURE CAHNGES</h1>
+      <h1>FEATURE CHANGES</h1>
     </div>
   );
 }
